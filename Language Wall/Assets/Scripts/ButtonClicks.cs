8d2c3e3cfef257d9a9ecc569a8aef1071bdf7382
William Brown <wbrown026@gmail.com>
2018-11-25 15:28:16 -0500

    Fixed Assets Folder

---
+++ Language Wall/Assets/Scripts/ButtonClicks.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClicks : MonoBehaviour {
+
+    public GameObject howToPanel; //The How To Panel of the title scene
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /**
+     * This function is called when the player clicks on the "Play" button in the title scene 
+     * When the "Play" button is clicked, it's goes to the game scene
+     * 
+     * @param scene - The name of the next scene
+     */
+    public void Play_Onclick(string scene)
+    {
+        Application.LoadLevel(scene);
+    }
+
+    /**
+     * This function is called when the player clicks on the "How to Play" button in the title scene.
+     * When the "How To Play" button is clicked, a new window opened up, telling the player instructions
+     * on how to play the game
+     */
+    public void HowToPlay_Onclick()
+    {
+        howToPanel.SetActive(true);
+    }
+}

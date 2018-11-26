bb7a8bde9a62223221e2da57ceb86f28bbc02d1c
William Brown <wbrown026@gmail.com>
2018-11-26 04:50:08 -0500

    Prototype setup
    Added a camera function and some assets

---
+++ Language Wall/Assets/Scripts/Camera/MouseFollowCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LW.Extentions;
+
+namespace LW.Cameras
+{
+    public class MouseFollowCamera : MonoBehaviour
+    {
+        #region Public Variables
+        public Vector2 bottomLeftCoordinate;
+        public Vector2 topRightCoordinate;
+        #endregion
+
+        #region Private Variables
+        private Vector2 _originCoordinate;
+        private Vector2 _topRightScreenCoordinate;
+        #endregion
+
+        #region Built In Methods
+        private void Start()
+        {
+            // Set Private Variables
+            _originCoordinate = new Vector2(0.0f, 0.0f);
+            _topRightScreenCoordinate = new Vector2(Screen.width, Screen.height);
+        }
+
+        private void Update()
+        {
+            // Update Camera Position
+            float newX = Input.mousePosition.x.Map(_originCoordinate.x, 
+                                                   bottomLeftCoordinate.x, 
+                                                   _topRightScreenCoordinate.x,
+                                                   topRightCoordinate.x);
+            float newY = Input.mousePosition.y.Map(_originCoordinate.y,
+                                                   bottomLeftCoordinate.y,
+                                                   _topRightScreenCoordinate.y,
+                                                   topRightCoordinate.y);
+            transform.position = new Vector3(newX, newY, transform.position.z);
+        }
+        #endregion
+    }
+}
---
+++ Language Wall/Assets/Scripts/Extensions/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LW.Extentions
+{
+    public static class ExtensionMethods
+    {
+        #region Methods
+        public static float Map(this float value, float from1, float to1, float from2, float to2)
+        {
+            //return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+            return to1 + (value - from1) * (to2 - to1) / (from2 - from1);
+        }
+        #endregion
+    }
+}
